b04d878b3e67fa705e97b80b4f535f81745aa26d
EmptyXNothing <Mr.Pisulkin666777@gmail.com>
2025-01-04 18:23:38 +0400

    Setting app

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// import { EditorState } from "@codemirror/state";
+// import { EditorView, keymap } from "@codemirror/view";
+// import { defaultKeymap } from "@codemirror/commands";
+// import { javascript } from "@codemirror/lang-javascript";
+
+// let startState = EditorState.create({
+//   doc: "Hello World",
+//   extensions: [keymap.of(defaultKeymap)],
+// });
+
+// let view = new EditorView({
+//   state: startState,
+//   parent: document.body,
+// });
+
+// console.log(view);
